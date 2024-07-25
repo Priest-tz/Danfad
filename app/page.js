8505d978dcabb887452d8dcf4ea94bdfb4703a5b
Dara <ajaoemmanuel01@gmail.com>
2024-07-25 08:41:47 +0100

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import Image from "next/image";
-import Hero from "./Home/page";
+import Hero from "./Hero/page";
+import Services from "./Components/Services";
+import Testimonials from "./Components/Testimonials";
+import EmailSub from "./Components/Emailsub";
+import Footer from "./Components/Footer";
 
 export default function Home() {
 	return (
-		<>
+		<div className="scroll-smooth">
 			<Hero />
-		</>
+			<Services />
+			<Testimonials />
+			<EmailSub />
+			<Footer />
+		</div>
 	);
 }
